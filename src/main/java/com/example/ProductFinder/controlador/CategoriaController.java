0275948e42e394170f87b0b06900b7c602860407
cesar jhoani <cesar.2092@hotmail.com>
2024-03-06 22:03:53 -0500

    la vista registro categoria y la logica registro categoria con exito

--- src/main/java/com/example/ProductFinder/controlador/CategoriaController.java
+++ src/main/java/com/example/ProductFinder/controlador/CategoriaController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -17,11 +19,25 @@ public class CategoriaController {
 
     @GetMapping("/categorias")
     public String listarCategorias(Model modelo){
-        List<Categoria> listaCategorias = categoriaRepository.findAll();
+        List<Categoria> listaCategorias = categoriaRepository.findAllByOrderByIdAsc();
         modelo.addAttribute("listaCategorias",listaCategorias);
         modelo.addAttribute("titulo","Listado de Categorias");
         return "categorias";
     }
 
+
+
+    @GetMapping("/categorias/nuevo")
+    public String mostrarFormularioDeNuevaCategoria(Model modelo){
+        Categoria categoria = new Categoria();
+        modelo.addAttribute("categoria",categoria);
+        return "registroCategoria";
+    }
+
+    @PostMapping("/categorias/guardar")
+    public String guardarCategoria(@ModelAttribute("categoria") Categoria categoria){
+        categoriaRepository.save(categoria);
+        return "redirect:/categorias";
+    }
 
 }
--- src/main/java/com/example/ProductFinder/repositorio/CategoriaRepository.java
+++ src/main/java/com/example/ProductFinder/repositorio/CategoriaRepository.java
@@ -3,6 +3,8 @@ package com.example.ProductFinder.repositorio;
 import com.example.ProductFinder.modelo.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoriaRepository extends JpaRepository<Categoria,Integer> {
+import java.util.List;
 
+public interface CategoriaRepository extends JpaRepository<Categoria,Integer> {
+    List<Categoria> findAllByOrderByIdAsc();// para que me liste en orden ya que aun no se por que se me viene en desorden los id
 }
