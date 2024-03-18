117d6de1128c7e5b78e96d931f9fe1a86e51bacc
cesar jhoani <cesar.2092@hotmail.com>
2024-03-18 01:14:29 -0500

    vista de registrar producto

--- src/main/java/com/example/ProductFinder/controlador/AdminProductoController.java
+++ src/main/java/com/example/ProductFinder/controlador/AdminProductoController.java
@@ -1,16 +1,19 @@
 package com.example.ProductFinder.controlador;
 
-import com.example.ProductFinder.modelo.Producto;
+import com.example.ProductFinder.modelo.*;
 import com.example.ProductFinder.repositorio.*;
 import com.example.ProductFinder.servicio.AlmacenServicioImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class AdminProductoController {
@@ -37,8 +40,21 @@ public class AdminProductoController {
         return "productos_admin";
     }
     @GetMapping("/agregarProducto")
-    public String mostrarFormularioDeNuevoProducto(){
-        return "";
+    public String mostrarFormularioDeNuevoProducto(Model model){
+        List<Sucursales> listaSucursales = sucursalRepository.findAll(Sort.by("nombre"));
+        List<Categoria> listaCategorias = categoriaRepository.findAll(Sort.by("nombre"));
+        List<Bodegas> listaBodegas = bodegaRepository.findAll(Sort.by("nombre"));
+        List<Modulo>  listaModulos = moduloRepository.findAll(Sort.by("nombre"));
+        List<Pasillo>  listaPasillos = pasilloRepository.findAll(Sort.by("nombre"));
+        model.addAttribute("listaSucursales",listaSucursales);
+        model.addAttribute("listaCategorias",listaCategorias);
+        model.addAttribute("listaBodegas",listaBodegas);
+        model.addAttribute("listaModulos",listaModulos);
+        model.addAttribute("listaPasillos",listaPasillos);
+        model.addAttribute("producto",new Producto());
+        return "registroProducto";
     }
+
+
 
 }
