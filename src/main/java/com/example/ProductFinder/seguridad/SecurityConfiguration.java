d4808a72a4f5efa3d32f05d726c64715cb3d7282
cesar jhoani <cesar.2092@hotmail.com>
2024-03-29 10:46:16 -0500

    modificacion en el frot-end de la vista login,titulo intermitente de buscar producto y agregacion de comentarios en la clase de seguridad de spring security

--- src/main/java/com/example/ProductFinder/seguridad/SecurityConfiguration.java
+++ src/main/java/com/example/ProductFinder/seguridad/SecurityConfiguration.java
@@ -21,13 +21,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     private UsuarioServicio  usuarioServicio;
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder(){ // indico como codifico la contraseña por que spring security espera una pasword
+    public BCryptPasswordEncoder passwordEncoder(){ // indico como codifico la contraseña por que spring security espera una pasword encriptada
         return new BCryptPasswordEncoder();
     }
     @Bean
-    public DaoAuthenticationProvider authenticationProvider(){
+    public DaoAuthenticationProvider authenticationProvider(){//provedor de autenticacion para autenticar los usuarios en una aplicación web mediante la autenticación basada en bases de datos.
         DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-        auth.setUserDetailsService(usuarioServicio);//le establesco el UserDetailsService para extraer los datos de un usuario y voy a tener sus datos roles, etc por que tiene el metodo loadUserByUsername() me carga un usuario
+        auth.setUserDetailsService(usuarioServicio);//le establesco el UserDetailsService a UsuarioServicio con extends para extraer los datos de un usuario y voy a tener sus datos roles, etc por que tiene el metodo loadUserByUsername() me carga un usuario
         auth.setPasswordEncoder(passwordEncoder());
         return auth;
     }
@@ -37,17 +37,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
         auth.authenticationProvider(authenticationProvider());
     }
 
-    @Override
+    @Override //sobrescribo el metodo configure que extiende WebSecurityConfigurerAdapter, la cual es una clase de utilidad para saber como deseamos configurar la seguridad
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(
                 "/registro**",
-                "/js**",
-                        "/error"
+                        "/error**"
                 ).permitAll()
+                //.antMatchers("/hola**").hasRole("ADMIN")
+                //.antMatchers("/hola").hasAuthority("OTROROL")  POR si quiere autorizar a un rol en especifico sin el porefijo ROLE_
+                //.antMatchers("/admin","/publico").hasRole("ADMIN")
                 .anyRequest().authenticated()// cualquier otra peticion vamos a autenticarla
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                //.defaultSuccessUrl("/inicio")  por si quiero que se redirija a una pagina por defecto despues de que inice sesion
                 .permitAll()
                 .and()
                 .logout()
